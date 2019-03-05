75140305d6ce6196951ceb0fb7ec193ca81e578b
Earl Balai Jr <earl@kiwi.io>
2019-03-05 17:26:14 +0700

    Added connection state and styles (incomplete) Refactor needed.

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,8 @@ class App extends Component {
       target: "en",
       speechLang: "en-US",
       autoSpeak: true,
-      voice: "Google US English"
+      voice: "Google US English",
+      connectedToNetwork: false
     }
     // Bind functions to make state available
     this.translate = this.translate.bind(this)
@@ -29,6 +30,7 @@ class App extends Component {
 
   // Perform translation and set state values
   translate() {
+    if (!this.state.connectedToNetwork) return
     axios
       .get(
         `https://translation.googleapis.com/language/translate/v2?target=${
@@ -93,56 +95,83 @@ class App extends Component {
 
   render() {
     return (
-      <div className='container p-5'>
-        <h6>Translation Experiment</h6>
-        <textarea
-          className='form-control mb-2'
-          placeholder='Write something here...'
-          value={this.state.value}
-          onChange={e => this.setState({ value: e.target.value })}
-        />
-        <div className='d-flex align-items-center'>
-          <button onClick={this.translate} className='btn btn-primary'>
-            Translate
-          </button>
-          <select
-            onChange={e => this.setLanguage(e)}
-            selected={this.state.target}
-            className='form-control ml-2'
-          >
-            {languageOptions.map(opt => (
-              <option key={opt.name} value={opt.code}>
-                {opt.name}
-              </option>
-            ))}
-          </select>
-          <div className='form-check w-100 ml-3'>
-            <input
-              type='checkbox'
-              checked={this.state.autoSpeak}
-              onChange={this.changeAutoSpeak}
-              className='form-check-input'
-            />
-            <label className='form-check-label'>Speak automatically</label>
-          </div>
-        </div>
-        {this.state.translated && (
-          <div className='mt-3'>
-            <h6 className='text-muted'>Translation</h6>
-            <div className='d-flex flex-column'>
-              <p onClick={this.speak} className='font-weight-bold text-success'>
-                {this.state.translated}
-              </p>
-            </div>
-            <small className='text-muted'>
-              Detected Language:
-              <b className='text-uppercase font-weight-bolder mr-2 ml-1'>
-                {this.state.source}
-              </b>
-              [Click the translation to hear it]
-            </small>
+      <div>
+        {!this.state.connectedToNetwork && (
+          <div className='connection-banner text-monospace bg-danger'>
+            You are not connected to the internet
           </div>
         )}
+        <div className='container p-5'>
+          <div className='d-flex align-items-center mb-3'>
+            <h6 className='m-0 p-0'>Translation Experiment</h6>
+            <div className='ml-auto d-flex align-items-center'>
+              <small className='text-muted'>Network status</small>
+              <span className='connection-state bg-danger' />
+            </div>
+          </div>
+          <textarea
+            className={
+              this.state.connectedToNetwork
+                ? "form-control mb-2"
+                : "form-control mb-2 disabled"
+            }
+            placeholder='Write something here...'
+            value={this.state.value}
+            onChange={e => this.setState({ value: e.target.value })}
+          />
+          <div className='d-flex align-items-center'>
+            <button
+              onClick={this.translate}
+              className={
+                this.state.connectedToNetwork
+                  ? "btn btn-primary"
+                  : "btn btn-primary disabled"
+              }
+            >
+              Translate
+            </button>
+            <select
+              onChange={e => this.setLanguage(e)}
+              selected={this.state.target}
+              className='form-control ml-2'
+            >
+              {languageOptions.map(opt => (
+                <option key={opt.name} value={opt.code}>
+                  {opt.name}
+                </option>
+              ))}
+            </select>
+            <div className='form-check w-100 ml-3'>
+              <input
+                type='checkbox'
+                checked={this.state.autoSpeak}
+                onChange={this.changeAutoSpeak}
+                className='form-check-input'
+              />
+              <label className='form-check-label'>Speak automatically</label>
+            </div>
+          </div>
+          {this.state.translated && (
+            <div className='mt-3'>
+              <h6 className='text-muted'>Translation</h6>
+              <div className='d-flex flex-column'>
+                <p
+                  onClick={this.speak}
+                  className='font-weight-bold text-success'
+                >
+                  {this.state.translated}
+                </p>
+              </div>
+              <small className='text-muted'>
+                Detected Language:
+                <b className='text-uppercase font-weight-bolder mr-2 ml-1'>
+                  {this.state.source}
+                </b>
+                [Click the translation to hear it]
+              </small>
+            </div>
+          )}
+        </div>
       </div>
     )
   }
